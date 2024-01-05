c8744d7fd64578bb5433c84e5283617fbd0bf723
Samuel <samuel@devsquad.com>
2024-01-04 23:10:34 -0300

    add mocked table

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter    = require('./routes/index');
-var usersRouter    = require('./routes/users');
-var studentsRouter = require('./routes/students');
+const indexRouter    = require('./routes/index');
+const usersRouter    = require('./routes/users');
+const studentsRouter = require('./routes/students');
 
 var app = express();
 
